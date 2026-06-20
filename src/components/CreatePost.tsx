dc5e0a9b19080314c92d809bbc6ea195ac1a92ff
Akash Randhawa <93700117+akashrandhawa00@users.noreply.github.com>
2026-06-20 13:51:02 -0400

    create post page and logic was added

--- src/components/CreatePost.tsx
+++ src/components/CreatePost.tsx
@@ -1,5 +1,5 @@
 import { useMutation } from "@tanstack/react-query";
-import { useState } from "react";
+import { useState, type ChangeEvent } from "react";
 import { supabase } from "../supabase-client";
 
 interface PostInput {
@@ -7,8 +7,25 @@ interface PostInput {
     content: string;
 }
 
-const createPost = async (post: PostInput) => {
-    const { data, error } = await supabase.from("posts").insert([post]);
+const createPost = async (post: PostInput, imageFile: File) => {
+    // upload image first and get the url
+    const filePath = `${post.title.split(" ").join("_")}-${Date.now()}-${imageFile.name}`;
+
+    const { error: uploadError } = await supabase.storage
+        .from("post-images")
+        .upload(filePath, imageFile);
+
+    if (uploadError) throw new Error(uploadError.message);
+
+    const { data: publicUrlData } = supabase.storage
+        .from("post-images")
+        .getPublicUrl(filePath);
+
+    //use the image url and then create the post
+
+    const { data, error } = await supabase
+        .from("posts")
+        .insert({ ...post, image_url: publicUrlData.publicUrl });
 
     if (error) throw new Error(error.message);
 
@@ -20,35 +37,75 @@ const createPost = async (post: PostInput) => {
 export const CreatePost = () => {
     const [title, setTitle] = useState<string>("");
     const [content, setContent] = useState<string>("");
+    const [selectedFile, setSelectedFile] = useState<File | null>(null);
 
-    const {} = useMutation({ mutationFn: createPost });
+    const { mutate, isPending, isError } = useMutation({
+        mutationFn: (data: { post: PostInput; imageFile: File }) => {
+            return createPost(data.post, data.imageFile);
+        },
+    });
 
     const handleSubmit = (event: React.FormEvent) => {
         event.preventDefault();
+
+        if (!selectedFile) {
+            return;
+        }
+        mutate({ post: { title, content }, imageFile: selectedFile });
+    };
+
+    const handleFileChange = (event: ChangeEvent<HTMLInputElement>) => {
+        if (event.target.files && event.target.files[0]) {
+            setSelectedFile(event.target.files[0]);
+        }
     };
 
     return (
-        <form>
+        <form onSubmit={handleSubmit} className="max-w-2xl mx-auto space-y-4">
             <div>
-                <label>Title</label>
+                <label htmlFor="title" className="block mb-2 font-medium">
+                    Title
+                </label>
                 <input
                     type="text"
                     id="title"
                     required
                     onChange={(event) => setTitle(event.target.value)}
+                    className="w-full border border-white/10 bg-transparent p-2 rounded"
                 />
             </div>
             <div>
-                <label>Content</label>
+                <label htmlFor="content" className="block mb-2 font-medium">
+                    Content
+                </label>
                 <textarea
                     id="content"
                     required
                     rows={5}
-                    onChange={(event) => setTitle(event.target.value)}
+                    onChange={(event) => setContent(event.target.value)}
+                    className="w-full border border-white/10 bg-transparent p-2 rounded"
+                />
+            </div>
+            <div>
+                <label className="block mb-2 font-medium">Upload Image</label>
+                <input
+                    type="file"
+                    id="image"
+                    accept="image/*"
+                    required
+                    onChange={handleFileChange}
+                    className="w-full text-gray-200"
                 />
             </div>
 
-            <button type="submit">Create Post</button>
+            <button
+                type="submit"
+                className="bg-purple-500 text-white px-4 py-2 rounded cursor-pointer"
+            >
+                {isPending ? "Creating..." : "Create Post"}
+            </button>
+
+            {isError && <p className="text-red-500">Error creating post.</p>}
         </form>
     );
 };
